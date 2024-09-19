3d97f233e979ecf6fa801011e83aeae0c8fc33a1
Keyner Lizarazo <keynerlizarazo655@gmail.com>
2024-09-19 11:01:00 -0500

    Optimizacion del Reporte de PDF
    -Se verifica en base al contenido para evitar sobreescritura y elimina redundancias
    -cambio en el formato del nombre

--- backend/Reporte de PDF.py
+++ backend/Reporte de PDF.py
@@ -1,6 +1,7 @@
 from fpdf import FPDF   #SI NO LES FUNCIONA DEBEN INSTALAR la libreria fpdf
 from datetime import datetime, timedelta
 import os
+import filecmp#Libreria para comparar archivos
 
 def obtener_nombre_archivo_unico(nombre_archivo_base):#Este funcion evita lo sobreescritura de un mismo archivo con el uso de un contador
     contador = 1
@@ -9,6 +10,24 @@ def obtener_nombre_archivo_unico(nombre_archivo_base):#Este funcion evita lo sob
         nombre_archivo = f"{nombre_archivo_base}({contador}).pdf"
         contador += 1
     return nombre_archivo
+
+# Función para verificar y renombrar el archivo si es necesario
+def verificar_y_guardar_pdf(pdf, nombre_archivo_base):
+    nombre_archivo_temporal = "temp_reporte.pdf"
+    pdf.output(nombre_archivo_temporal, 'F')
+    
+    nombre_archivo_final = obtener_nombre_archivo_unico(nombre_archivo_base)
+    if os.path.exists(nombre_archivo_final):
+        if filecmp.cmp(nombre_archivo_temporal, nombre_archivo_final, shallow=False):#Si los archivos son idénticos, se elimina el archivo temporal y se imprime un mensaje indicando que el archivo ya existe y su contenido es idéntico.
+            os.remove(nombre_archivo_temporal)
+            print("El archivo ya existe y su contenido es idéntico.")
+        else:
+            os.rename(nombre_archivo_temporal, nombre_archivo_final)#renombra el archivo
+            print(f"Archivo guardado como {nombre_archivo_final}")
+    else:
+        os.rename(nombre_archivo_temporal, nombre_archivo_final)
+        print(f"Archivo guardado como {nombre_archivo_final}")
+
 
 class Cliente:
     def __init__(self, nombre, telefono, cedula, direccion):
@@ -112,8 +131,8 @@ cliente = Cliente("Keyner Ivan Lizarazo Diaz", "04263757236", "30905297", "Las M
 # Crear instancias de libros ficticios
 libro1 = Libro("12345", "Ficción", "Sala A", "Literatura", "001", "Gabriel García Márquez", "Cien Años de Soledad", "1", "3", "Primera", "1967", "Editorial Sudamericana")
 libro2 = Libro("D221", "Fantasia", "General","Literatura", 123.323, "Brayan Diaz", "Historias Medievales",2, 2, 12, 1998, "Libertadores" )
-libros_prestamos = [libro1,libro2]
-
+libros_prestamos1 = [libro1,libro2]
+libros_prestamos2 = [libro2,libro1]
 # Fecha de registro y fecha límite
 fecha_registro = datetime.now().strftime("%d/%m/%Y")
 fecha_limite = (datetime.now() + timedelta(days=20)).strftime("%d/%m/%Y")
@@ -124,18 +143,18 @@ pdf.agregar_datos_cliente(cliente)
 pdf.agregar_datos_libro(libro1, fecha_registro, fecha_limite)
 pdf.agregar_firmas() 
 
-# for indice,libro in enumerate(libros_prestamos): # De esta forma se desempaqueta el contenido de la lista y genera las hojas necesarias
-#     if indice > 0:#De esta forma se crea la pagina antes de entrar al bucle y no crear una pagina adicional
-#         pdf.add_page()
-#     pdf.agregar_datos_cliente(cliente)
-#     pdf.agregar_datos_libro(libro, fecha_registro, fecha_limite)
-#     pdf.agregar_firmas() 
+for indice,libro in enumerate(libros_prestamos2): # De esta forma se desempaqueta el contenido de la lista y genera las hojas necesarias
+     if indice > 0:#De esta forma se crea la pagina antes de entrar al bucle y no crear una pagina adicional
+         pdf.add_page()
+     pdf.agregar_datos_cliente(cliente)
+     pdf.agregar_datos_libro(libro, fecha_registro, fecha_limite)
+     pdf.agregar_firmas() 
 
 
 
 fecha_actual = datetime.now().strftime("%d_%m_%Y")#Aqui se usa un guion bajo para evitar errores con caracteres espéciales en el nombre del documento
-titulo_corregido= libro1.titulo.replace(" ","_")#De esta forma se remplazan los espacios en blanco para evitar errores al crear el archivo
-nombre_archivo_base= f"Reporte_{titulo_corregido}_{fecha_actual}.pdf"
+nombre_corregido = cliente.nombre.replace(" ","_")#De esta forma se remplazan los espacios en blanco para evitar errores al crear el archivo
+nombre_archivo_base= f"Reporte_de_prestamo_{nombre_corregido}_{fecha_actual}"
 
 #Establecer metadatos del documento
 pdf.set_title("Reporte de Libro")
@@ -145,4 +164,4 @@ pdf.set_subject("Reporte de préstamo de libros")
 pdf.set_keywords("Reporte,Libro,Préstamo,Biblioteca")#Estas palabras clave pueden ser usadas por los motores de búsqueda y los lectores de PDF para indexar y encontrar el documento más fácilmente.
 
 nombre_archivo = obtener_nombre_archivo_unico(nombre_archivo_base)
-pdf.output(nombre_archivo, 'F')
+verificar_y_guardar_pdf(pdf,nombre_archivo_base)
